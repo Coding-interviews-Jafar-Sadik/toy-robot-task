7d86ddb94aa6b9e50a92af467a2ce9f3f20fb0ef
Jafar Sadik <dzafar.sadik@gmail.com>
2019-12-07 13:20:42 +0000

    Simplified ToyRobot branching logic with a state pattern

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Github CI pipeline runs tests after every commit
 
 The project uses Lombok to reduce boilerplate code. Please install Lombok plugin for your favourite IDE and enable 'annotation processing' option.
 
-Notes:
-- ToyRobot could implement a state pattern to simplify branching logic and avoid checking 'robotOnTable' flag every time.  
-- alternatively state update could be executed directly in command.execute() method. This would allow to test commands independently, 
-implement undo functionality but comes with a greater code complexity which is an overkill for this task. 
+Notes:  
+- Currently ToyRobot class implements state pattern to simplify branching logic and avoid checking boolean flag every time. 
+Alternatively state update could be executed directly in command.execute() method. This would allow to test commands independently, 
+implement undo functionality but comes with a greater code complexity which, I believe, is an overkill for this task. 
 - the code has been checked with Sonar Lint and therefore some false warnings were suppressed with @SuppressWarnings
 
 This is a solution to 'Toy Robot' task: 
--- src/main/java/org/toyrobot/ToyRobot.java
+++ src/main/java/org/toyrobot/ToyRobot.java
@@ -2,20 +2,20 @@ package org.toyrobot;
 
 import org.toyrobot.commands.AbstractCommand;
 import org.toyrobot.commands.CommandParser;
+import org.toyrobot.commands.CommandType;
 import org.toyrobot.commands.PlaceCommand;
 import org.toyrobot.math.Direction;
 import org.toyrobot.math.Point2D;
 import org.toyrobot.runtime.DefaultRuntime;
 import org.toyrobot.runtime.Runtime;
 
-import static org.toyrobot.commands.CommandType.*;
 import static org.toyrobot.math.Point2D.point2d;
 
 public class ToyRobot {
     private final CommandParser commandParser = new CommandParser();
     private final Runtime runtime;
 
-    private boolean robotOnTable = false;
+    private State currentState = new RobotNotYetOnTable();
     private Point2D position = point2d(0, 0);
     private Direction direction = Direction.UNKNOWN;
 
@@ -28,25 +28,7 @@ public class ToyRobot {
     }
 
     public void execute(String command) {
-        execute(commandParser.parse(command));
-    }
-
-    private void execute(AbstractCommand command) {
-        if (command.getType() == PLACE) {
-            PlaceCommand placeCommand = (PlaceCommand) command;
-            update(placeCommand.getPosition(), placeCommand.getDirection());
-        } else if (robotOnTable) {
-            if (command.getType() == MOVE) {
-                updatePosition(position.add(direction.vector()));
-            } else if (command.getType() == LEFT) {
-                updateDirection(direction.rotateAntiClockwise());
-            } else if (command.getType() == RIGHT) {
-                updateDirection(direction.rotateClockwise());
-            } else if (command.getType() == REPORT) {
-                String report = String.format("=> Output: %d,%d,%s", position.getX(), position.getY(), direction.name());
-                runtime.print(report);
-            }
-        }
+        currentState.execute(commandParser.parse(command));
     }
 
     public Point2D getPosition() {
@@ -57,26 +39,75 @@ public class ToyRobot {
         return direction;
     }
 
+    private class RobotNotYetOnTable implements State {
+        @Override
+        public void execute(AbstractCommand command) {
+            if (command.getType() == CommandType.PLACE) {
+                PlaceCommand placeCommand = (PlaceCommand) command;
+                if (update(placeCommand.getPosition(), placeCommand.getDirection())) {
+                    currentState = new RobotOnTable();
+                }
+            }
+        }
+    }
+
+    private class RobotOnTable implements State {
+        @Override
+        public void execute(AbstractCommand command) {
+            switch (command.getType()) {
+                case PLACE:
+                    PlaceCommand placeCommand = (PlaceCommand) command;
+                    update(placeCommand.getPosition(), placeCommand.getDirection());
+                    break;
+
+                case MOVE:
+                    updatePosition(position.add(direction.vector()));
+                    break;
+
+                case LEFT:
+                    updateDirection(direction.rotateAntiClockwise());
+                    break;
+
+                case RIGHT:
+                    updateDirection(direction.rotateClockwise());
+                    break;
+
+                case REPORT:
+                    String report = String.format("=> Output: %d,%d,%s", position.getX(), position.getY(), direction.name());
+                    runtime.print(report);
+                    break;
+
+                default:
+            }
+        }
+
+        private void updateDirection(Direction newDirection) {
+            direction = newDirection;
+        }
+
+        private void updatePosition(Point2D newPosition) {
+            if (inBounds(newPosition)) {
+                position = newPosition;
+            }
+        }
+    }
+
+    private interface State {
+        void execute(AbstractCommand command);
+    }
+
     private boolean inBounds(Point2D point) {
         final int tableSize = 5;
         return point.getX() >= 0 && point.getY() >= 0 && point.getX() < tableSize && point.getY() < tableSize;
     }
 
-    private void updateDirection(Direction newDirection) {
-        this.direction = newDirection;
-    }
-
-    private void updatePosition(Point2D newPosition) {
-        if (inBounds(newPosition)) {
-            this.position = newPosition;
-        }
-    }
-
-    private void update(Point2D newPosition, Direction newDirection) {
+    private boolean update(Point2D newPosition, Direction newDirection) {
+        boolean updated = false;
         if (inBounds(newPosition) && newDirection != Direction.UNKNOWN) {
             this.position = newPosition;
             this.direction = newDirection;
-            this.robotOnTable = true;
+            updated = true;
         }
+        return updated;
     }
 }
